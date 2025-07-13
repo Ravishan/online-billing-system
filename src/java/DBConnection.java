3eb5673dfe150a99ad54166d019226b00ad2a18f
ravishan99 <ravishan99@localhost>
2025-07-13 15:13:40 +0530

    Initial Commit

---
+++ src/java/DBConnection.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+public class DBConnection {
+    public static Connection getConnection() {
+        Connection connection = null;
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            connection = DriverManager.getConnection(
+                "jdbc:mysql://localhost:3306/online_billing", 
+                "root", 
+                ""  // No password for XAMPP default
+            );
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return connection;
+    }
+}
